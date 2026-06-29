ab1b032b9e939711385483b50762645db9256d92
Suraj Vishwakarma <surajv7@LL-1515L.kpit.com>
2026-06-29 20:46:41 +0530

    Source Code

---
+++ build/CMakeFiles/app_kernel_syslogng.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app_kernel_syslogng.dir/src/main.cpp.o"
+  "CMakeFiles/app_kernel_syslogng.dir/src/main.cpp.o.d"
+  "app_kernel_syslogng"
+  "app_kernel_syslogng.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app_kernel_syslogng.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/app_kernel_syslogng.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for app_kernel_syslogng.
+# This may be replaced when dependencies are built.
